0ba2d3fb8b715518b18cf2e6de25988c4061e07e
Gustavo Zeni <gus_ghz@live.com>
2022-09-17 01:42:59 -0300

    Lab 2 quase terminado - bug na variavel que recebe SysTickValueGet

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -60,6 +60,7 @@ using std::endl;
 #include "driverlib/pin_map.h"
 #include "driverlib/systick.h"
 #include "driverlib/sysctl.h"
+#include "../../TivaWare_C_Series-2.2.0.295/inc/tm4c1294ncpdt.h"
 
 
 /*------------------------------------------------------------------------------
@@ -76,6 +77,7 @@ using std::endl;
 	volatile uint32_t tempo = 0;
 	volatile uint32_t timer = 0;
         volatile uint32_t chave = 0;
+        uint32_t max_tick = 30000000;
 	uint32_t ui32SysClock; //Clock em Hz
 
 /*------------------------------------------------------------------------------
@@ -89,6 +91,11 @@ using std::endl;
  *      Functions and Methods
  *
  *------------------------------------------------------------------------------*/
+        
+//Declarations
+void initGPIO(void);
+void initInterrupt(void);
+//void GPIOJIntHandler(void);
 
 extern void SysTickIntHandler(void)
 {
@@ -98,15 +105,14 @@ extern void SysTickIntHandler(void)
 
 extern void GPIOJIntHandler(void)
 {	
-	// Set GPIO high to indicate entry to this interrupt handler.
-    GPIOPinWrite(GPIO_PORTJ_BASE, GPIO_PIN_0, GPIO_PIN_0);
+    // Set GPIO high to indicate entry to this interrupt handler.
+    //GPIOPinWrite(GPIO_PORTJ_BASE, GPIO_PIN_0, GPIO_PIN_0);
 
-	SysTickDisable();
-	chave = 1;
+    SysTickDisable();
+    chave = 1;
 	
-
     // Set GPIO low to indicate exit from this interrupt handler.
-    MAP_GPIOPinWrite(GPIO_PORTJ_BASE, GPIO_PIN_0, 0);
+    //MAP_GPIOPinWrite(GPIO_PORTJ_BASE, GPIO_PIN_0, 0);
 }
 /**
  * Main function.
@@ -117,26 +123,16 @@ extern void GPIOJIntHandler(void)
  */
 int main(int argc, char ** argv)
 {
-          uint32_t max_tick = 120000000/4;
 
-      ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
-                                       SYSCTL_OSC_MAIN |
-                                       SYSCTL_USE_PLL |
-                                       SYSCTL_CFG_VCO_240), 120000000);
 
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION); //LED
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ); //Switch
-	while(!(SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)&&SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ))){}
+  ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
+                                   SYSCTL_OSC_MAIN |
+                                   SYSCTL_USE_PLL |
+                                   SYSCTL_CFG_VCO_240), 120000000);
 
-	GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_1);
-	GPIODirModeSet(GPIO_PORTJ_BASE, GPIO_PIN_0, GPIO_DIR_MODE_IN);
-        GPIOPadConfigSet(GPIO_PORTJ_BASE, GPIO_PIN_0,GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
-        
-	IntMasterEnable();
-        SysTickIntEnable();
-        GPIOIntTypeSet(GPIO_PORTJ_BASE, GPIO_PIN_0, GPIO_LOW_LEVEL);
-        IntEnable(INT_GPIOJ_TM4C129);
-
+  initGPIO();
+  initInterrupt();
+  
 	
   while (1)
   {
@@ -153,12 +149,16 @@ int main(int argc, char ** argv)
 	GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0x0); //LED Desligado
 	if (chave)
         {
+          // Current iteration 
           uint32_t valor = SysTickValueGet();
-          tempo = timer * max_tick;
-          tempo += valor;
-          cout <<"Tempo de resposta: ";
-          cout << tempo;
-          cout << "\n";
+          tempo = timer * max_tick + valor;
+          //tempo += valor;
+          //double aux = 0;
+          //cout << "timer: " << timer << " max_tick: " << max_tick << "\n";
+          //cout << "valor: " << valor << " tempo: " << tempo << "\n";
+          //aux = tempo/120000000;
+          //cout << aux << "\n";
+          cout <<"Tempo de resposta: " << (tempo/120000000) << "ms" << "\n";
         }
         else
         {
@@ -167,3 +167,75 @@ int main(int argc, char ** argv)
         }
   }
 }	
+
+void initGPIO(void) {
+  
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION); //LED
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ); //Switch
+  while(!(SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)&&SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ))){}
+
+  GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_1);
+  GPIODirModeSet(GPIO_PORTJ_BASE, GPIO_PIN_0, GPIO_DIR_MODE_IN);
+  GPIOPadConfigSet(GPIO_PORTJ_BASE, GPIO_PIN_0,GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+  
+
+  SysTickIntEnable();
+  
+  /*
+  // Enable the GPIO port that is used for the on-board LED.
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+
+  // Check if the peripheral access is enabled.
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))
+  {
+  }
+
+  // Enable the GPIOJ peripheral
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);
+  
+  // Wait for the GPIOJ module to be ready.
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ))
+  {
+  }
+  
+  // Enable the GPIO pin for the LED (PN0).  Set the direction as output, and
+  // enable the GPIO pin for digital function.
+  GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+  
+  // Enable the BTN pin (PJ0).  Set the direction as input, and
+  // enable the GPIO pin for digital function.
+  GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_0);    
+    
+  */
+}
+
+void initInterrupt()
+{
+ 
+   /*
+   IntMasterEnable();
+   GPIOIntTypeSet(GPIO_PORTJ_BASE, GPIO_PIN_0, GPIO_LOW_LEVEL);
+   IntEnable(INT_GPIOJ_TM4C129);
+   */
+
+   // Enables the specified GPIO interruputs
+   GPIOIntEnable(GPIO_PORTJ_BASE, GPIO_INT_PIN_0);
+  
+   // Set the specified type in the specified pin (high_level/low_level),
+   // (falling_edge/rising_edge)
+   GPIOIntTypeSet(GPIO_PORTJ_BASE, GPIO_PIN_0, GPIO_LOW_LEVEL);
+   
+   // Enable the interrupt for PORTJ.
+   IntEnable(INT_GPIOJ);
+   
+   // Set the priority
+   IntPrioritySet(INT_GPIOJ, 0x00);
+   
+   // Register the handler for GPIOJ interrupts
+   //IntRegister(INT_GPIOJ, GPIOJIntHandler);
+   
+   IntMasterEnable(); // Enable the Master key of interrupt.
+    
+}
+
+
